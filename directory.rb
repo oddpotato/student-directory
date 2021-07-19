13eeaece6f240c6fe5411f3f41ebf7663b8305f5
Fiona Apps <wikipanyd@gmail.com>
2021-07-19 10:37:50 +0100

    Further refactoring code with methods

--- directory.rb
+++ directory.rb
@@ -14,12 +14,22 @@ students = [
 "Norman Bates"
 ]
 
-puts "The students of Villain's Academy"
-puts "--------------"
-students.each do |student|
-    puts student
+def print_header
+    puts "The students of Villain's Academy"
+    puts "--------------"
+end
+
+def print(names)
+    names.each do |name|
+      puts name
+    end
+end
+
+def print_footer(names)
+    puts "Overall, we have #{names.count} great students"
 end
 
 # This print statement states how many students
-# there are at the school. Output is one line.
-print "Overall, we have #{students.count} great students"
+print_header
+print(students)
+print_footer(students)
