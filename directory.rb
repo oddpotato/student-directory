a45c31b04213e95b89c743d63c698b3d9f3ef941
Fiona Apps <wikipanyd@gmail.com>
2021-07-19 10:34:06 +0100

    updating with refactored code

--- directory.rb
+++ directory.rb
@@ -1,19 +1,25 @@
 student_count = 11
 # This puts the student roster on individual lines
-puts "The students of the Villains Academy"
-puts "-------------"
-puts "Dr Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+students = [ 
+"Dr Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex DeLarge",
+"The Wicked Witch of the West",
+"Terminator",
+"Freddy Krueger",
+"The Joker",
+"Joffrey Baratheon",
+"Norman Bates"
+]
+
+puts "The students of Villain's Academy"
+puts "--------------"
+students.each do |student|
+    puts student
+end
 
 # This print statement states how many students
 # there are at the school. Output is one line.
-print "Overall, we have #{student_count} great students"
+print "Overall, we have #{students.count} great students"
