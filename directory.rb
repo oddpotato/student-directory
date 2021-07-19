7a6f4070cbd6ddae6503799ff7e82fa9cca6028e
Fiona Apps <wikipanyd@gmail.com>
2021-07-19 10:09:16 +0100

    adding comment on code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# This puts the student roster on individual lines
 puts "The students of the Villains Academy"
 puts "-------------"
 puts "Dr Hannibal Lecter"
@@ -12,6 +13,8 @@ puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
 
+# This print statement states how many students
+# there are at the school. Output is one line.
 print "Overall, we have "
 print 11
 print " great students"
